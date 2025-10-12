17007ce12772e1f9ffdc23c17a63e94bc8627acd
Mustafa Ghazi <ghazimostafa10@icloud.com>
2025-10-12 19:53:23 +0200

    no message

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -4,9 +4,7 @@ import 'package:doctor_app/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-/* 
-1//03XPmeV33HP1MCgYIARAAGAMSNwF-L9Irf7SbAnxliQpFDMOQIZOV206Zr7pDYU-dy4oj7fH-gpwBo9-GjWujohvWvPgNLuNRUdM
-*/
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetit();
